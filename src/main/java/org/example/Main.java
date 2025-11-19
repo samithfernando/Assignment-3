cc69ee549684cfee052c33cc4d0e1a1d0b56babc
samith <samithfernando539@gmail.com>
2025-11-19 13:24:08 -0500

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+
+public class Main {
+    static void main() {
+        FilePlayer player = new FilePlayer();
+        player.play("src/main/resources/sounds/fa.wav");
+    }
+}
