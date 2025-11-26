285ca99a1483c1e97d592645ccbcce31155cb22d
Adrian Antoniw <aanto081@uottawa.ca>
2025-11-26 16:38:53 -0500

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,11 @@ package org.example;
 
 
 
-
-/* ---------------------- */
-/* Thread 1               */
-/* ---------------------- */
 class Thread1 extends Thread {
     private final FilePlayer player;
     private final SongPosition pos;
-
     private final String[] notes = {"src/main/resources/sounds/do.wav", "src/main/resources/sounds/mi.wav", "src/main/resources/sounds/sol.wav", "src/main/resources/sounds/si.wav", "src/main/resources/sounds/do-octave.wav"};
-
     private final int[] indices = {0, 2, 4, 6, 7};
-
     private final int[] currentSong;
 
     public Thread1(FilePlayer player, SongPosition pos, int[] currentSong) {
@@ -22,27 +15,20 @@ class Thread1 extends Thread {
         this.currentSong = currentSong;
     }
 
-
-
     @Override
     public void run() {
-        for (int i = 0; i < notes.length; i++) {
-            Main.playNote(player, notes[i], indices[i], pos, currentSong);
+        while (pos.get() < currentSong.length) {
+            Main.playNote(player, notes, indices, pos, currentSong);
         }
     }
 }
 
-/* ---------------------- */
-/* Thread 2               */
-/* ---------------------- */
+
 class Thread2 extends Thread {
     private final FilePlayer player;
     private final SongPosition pos;
-
     private final String[] notes = {"src/main/resources/sounds/re.wav", "src/main/resources/sounds/fa.wav", "src/main/resources/sounds/la.wav", "src/main/resources/sounds/do-octave.wav"};
-
     private final int[] indices = {1, 3, 5, 7};
-
     private final int[] currentSong;
 
     public Thread2(FilePlayer player, SongPosition pos, int[] currentSong) {
@@ -53,63 +39,66 @@ class Thread2 extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < notes.length; i++) {
-            Main.playNote(player, notes[i], indices[i], pos, currentSong);
+        while (pos.get() < currentSong.length) {
+            Main.playNote(player, notes, indices, pos, currentSong);
         }
     }
 }
 
+
 class SongPosition {
     private int index = 0;
-
-    public int get() { return index; }
-    public void next() { index++; }
-    public void reset() { index = 0; }
+    public int get() {return index;}
+    public void next() {index++;}
 }
 
 
 public class Main {
-
     private static final int[][] SONGS = {{0,1,2,3,4,5,6,7}, {0,0,4,4,5,5,4,3,3,2,2,1,1,0,4,4,3,3,2,2,1,4,4,3,3,2,2,1,0,0,4,4,5,5,4,3,3,2,2,1,1,0}};
 
-
-    public static synchronized void playNote(FilePlayer player, String note, int noteIndex, SongPosition pos, int[] song) {
-        while (pos.get() < song.length && song[pos.get()] != noteIndex) {
-            try {
-                Main.class.wait();
-            } catch (InterruptedException ignored) {}
-        }
-
+    public static synchronized void playNote(FilePlayer player, String[] notes, int[] indices, SongPosition pos, int[] song) {
+        int expected = song[pos.get()];
         if (pos.get() >= song.length) {
             return;
         }
 
-        player.play(note);
-        pos.next();
-        Main.class.notifyAll();
+        for (int i = 0; i < indices.length; i++) {
+            if (indices[i] == expected) {
+                player.play(notes[i]);
+                //System.out.println("Playing note: " + notes[i]);
+                pos.next();
+                Main.class.notifyAll();
 
-        try { Thread.sleep(500); }
-        catch (InterruptedException ignored) {}
+                try {
+                    Thread.sleep(500);
+                } catch (Exception ignored) {}
+                return;
+            }
+        }
+
+        try {
+            Main.class.wait();
+        } catch (InterruptedException ignored) {}
     }
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         FilePlayer player = new FilePlayer();
-        SongPosition pos = new SongPosition();
 
         for (int s = 0; s < SONGS.length; s++) {
+            SongPosition pos = new SongPosition();
             int[] currentSong = SONGS[s];
-
-            System.out.println("playing sont:"+s);
+            System.out.println("Playing Task: #" + (s+1));
             Thread1 t1 = new Thread1(player, pos, currentSong);
             Thread2 t2 = new Thread2(player, pos, currentSong);
-
-            pos.reset();
 
             t1.start();
             t2.start();
 
-
+            try {
+                t1.join();
+                t2.join();
+            } catch (InterruptedException ignored) {}
         }
     }
 }
